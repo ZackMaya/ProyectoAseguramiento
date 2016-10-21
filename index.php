cb2662b0bcba3293f5f6266b7eaf362ba6cc5ad1
Ernesto Maya <ZackMaya@users.noreply.github.com>
2016-10-20 22:46:12 -0500

    Se integro la vista realizada por Erick
    Se actualización todos los archivos php, con las vistas y el pie de pagina de Erick

---
+++ index.php
@@ -0,0 +1,197 @@
+<?php
+	session_start();
+	session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <title>INTITUCIONES</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="css/estilo.css">
+  <!--Query-->
+  <script src="http://code.jquery.com/jquery-2.1.4.js"></script>
+
+</head>
+<body background="">
+  <header>
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+      <div class="container">
+
+        <div id="menuprincipal">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion">
+            <span class="sr-only">Desplegar / Ocultar Menú</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="index.php" class="navbar-brand">Consulta Instituciones</a>
+        </div>
+        </div>
+
+        <!-- MENU -->
+        <div class="collapse navbar-collapse" id="navegacion">
+          <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toogle" data-toggle="dropdown" role="button">
+                  Categorias<span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Requisitos de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Diseño de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Construcción de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Pruebas de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Mantenimiento de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Gestión de la configuración</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Gestión de la Ingeniería de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Proceso de Ingeniería de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Herramientas y métodos de la Ingeniería de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Calidad del Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Práctica Profesional de la Ingeniería de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Economía de la Ingeniería de Software</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Fundamentos de Computación</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Fundamentos Matemáticos</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Fundamentos de Ingeniería</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav">
+              <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                  Administrador<span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="./php/loginAdmin.php">Iniciar sesión</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                  Usuario<span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="./php/registrousuario.php">Registrarse</a></li>
+                  <li><a href="./php/sesion.php">Iniciar Sesión</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toogle " data-toggle="dropdown" role="button">
+                  Acerca de.. <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Ayuda</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Información</a></li>
+                </ul>
+              </li>
+            </ul>
+
+ 
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <section class="jumbotron">
+    <div class="container" align="center">
+      <a href="#" class="thumb">
+        <img src="Images/LogoUACMC.jpg" width="10%" height="10%" >
+        <img src="Images/Logo15.png" width="20%" height="20%">
+      </a>
+    </div>
+  </section><br><br><br>
+
+    <!--Contenido-->
+    <section class="main container">
+      <div class="row">
+        <aside class="col-md-3 hidden-xs hidden-sm">
+          <h4>Categorias</h4>
+          <div class="list-group">
+            <a href="#" class="list-group-item">Requisitos de Software</a>
+            <a href="#" class="list-group-item">Diseño de Software</a>
+            <a href="#" class="list-group-item">Construcción de Software</a>
+            <a href="#" class="list-group-item">Pruebas de Software</a>
+            <a href="#" class="list-group-item">Mantenimiento de Software</a>
+            <a href="#" class="list-group-item">Gestión de la configuración</a>
+            <a href="#" class="list-group-item">Gestión de la Ingeniería de Software</a>
+            <a href="#" class="list-group-item">Proceso de Ingeniería de Software</a>
+            <a href="#" class="list-group-item">Herramientas y métodos de la Ingeniería de Software</a>
+            <a href="#" class="list-group-item">Calidad del Software</a>
+            <a href="#" class="list-group-item">Práctica Profesional de la Ingeniería de Software</a>
+            <a href="#" class="list-group-item">Economía de la Ingeniería de Software</a>
+            <a href="#" class="list-group-item">Fundamentos de Computación</a>
+            <a href="#" class="list-group-item">Fundamentos Matemáticos</a>
+            <a href="#" class="list-group-item">Fundamentos de Ingeniería</a>
+          </div>
+        </aside>
+        <section class="posts col-md-9">
+          <article class="post clearfix">
+            <h2 class="post-title">Bienvenido</h2>
+            <p class="post-contenido text-justify">
+              En está aplicación podrás encontrar información relevante sobre intituciones relacionadas con las distintas materias que te encontraras a lo largo de la carrera, cualquier aclaración por favor de decirlas.
+            </p>
+          </article>
+
+          <article class="post clearfix">
+            <h2 class="post-title">Descripción categorias</h2>
+            <p class="post-contenido text-justify">
+              AQUÍ VA LAS SUBCATEGORIAS
+            </p>
+          </article>
+
+          <article class="post clearfix">
+            <h2 class="post-title">Descripción instituciones</h2>
+            <p class="post-contenido text-justify">
+              AQUÍ VA LA DESCRIPCION
+            </p>
+            <div class="contenedor-botones">
+              <a href="#" class="btn btn-primary">Comentarios</a>
+              <a href="login.html" class="btn btn-success">Editar</a>
+            </div>
+          </article>
+        </section>
+      </div>
+    </section>
+
+    <br><br><br>
+
+  
+<!-- Footer -->
+ <footer>
+      <div class="pie">
+        <div class="row">
+          <div class="col-xs-6">
+            <p>Aseguramiento de la Calidad del Software | UACM - SLT | 2016</p>
+          </div>
+          <div class="col-xs-6">
+            <ul class="list-inline text-right">
+              <li><a href="#">Contacto e-mail</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+</body>
+</html>
