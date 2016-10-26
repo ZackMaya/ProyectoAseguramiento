bf68872d65bfed8268fd74585ace077582f2097e
Ernesto Maya <ZackMaya@users.noreply.github.com>
2016-10-25 23:01:34 -0500

    Un script donde contiene todas las tablas
    Sugerencias se modifico la tabla pais agregando una columna y algunos nombres de las columnas para que en la programación sea más fácil, hay un script completo para llenar todos los países

---
+++ para_desarrolladores/scripts/instituciones2(1).sql
@@ -0,0 +1,455 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-10-2016 a las 03:58:34
+-- Versión del servidor: 5.7.13-log
+-- Versión de PHP: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `instituciones2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `areaswebook`
+--
+
+CREATE TABLE `areaswebook` (
+  `id_area` int(100) NOT NULL,
+  `nombrearea` varchar(45) NOT NULL,
+  `descripcion` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `areaswebook`
+--
+
+INSERT INTO `areaswebook` (`id_area`, `nombrearea`, `descripcion`) VALUES
+(1, 'Requisitos de Software', NULL),
+(2, 'Diseño de Software', NULL),
+(3, 'Construcción de Software', NULL),
+(4, 'Pruebas de Software', NULL),
+(5, 'Mantenimiento de Software', NULL),
+(6, 'Gestión de la configuración', NULL),
+(7, 'Gestión de la Ingeniería de Software', NULL),
+(8, 'Proceso de Ingeniería de Software', NULL),
+(9, 'Herramientas y métodos de la Ing Software', NULL),
+(10, 'Calidad del Software', NULL),
+(11, 'Práctica Profesional de la Ing Software', NULL),
+(12, 'Economía de la Ingeniería de Software', NULL),
+(13, 'Fundamentos de Computación', NULL),
+(14, 'Fundamentos Matemáticos', NULL),
+(15, 'Fundamentos de Ingeniería', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `institucion`
+--
+
+CREATE TABLE `institucion` (
+  `id_institucion` int(100) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  `area` int(100) NOT NULL,
+  `url_pagina` varchar(45) NOT NULL,
+  `descripcion` varchar(254) NOT NULL,
+  `url_imagen` varchar(45) DEFAULT NULL,
+  `pais` int(100) NOT NULL,
+  `autorizado` tinyint(1) DEFAULT NULL,
+  `ultima_actualizacion` date DEFAULT NULL,
+  `ultimo_en_actualizar` int(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `institucion`
+--
+
+INSERT INTO `institucion` (`id_institucion`, `nombre`, `telefono`, `correo`, `area`, `url_pagina`, `descripcion`, `url_imagen`, `pais`, `autorizado`, `ultima_actualizacion`, `ultimo_en_actualizar`) VALUES
+(3, 'SUM', '5557627896', 'sum@gmail.com', 1, 'sum.com.mx', 'En SUM apoyamos startups de alto impacto con incubación, aceleración, eventos, capacitación, asesoría, capital semilla, ángeles inversionistas, mentores, etc.', 'sumimage.com', 1, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `institucion_materia`
+--
+
+CREATE TABLE `institucion_materia` (
+  `id_institucion` int(100) NOT NULL,
+  `materia` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materia`
+--
+
+CREATE TABLE `materia` (
+  `id_materia` int(100) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `semestre` int(10) NOT NULL,
+  `descripcion` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE `pais` (
+  `id_pais` int(100) NOT NULL,
+  `ISO` varchar(45) NOT NULL,
+  `nombrepais` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pais`
+--
+
+INSERT INTO `pais` (`id_pais`, `ISO`, `nombrepais`) VALUES
+(1, 'AF', 'Afganistán'),
+(2, 'AX', 'Islas Gland'),
+(3, 'AL', 'Albania'),
+(4, 'DE', 'Alemania'),
+(5, 'AD', 'Andorra'),
+(6, 'AO', 'Angola'),
+(7, 'AI', 'Anguilla'),
+(8, 'AQ', 'Antártida'),
+(9, 'AG', 'Antigua y Barbuda'),
+(10, 'AN', 'Antillas Holandesas'),
+(11, 'SA', 'Arabia Saudí'),
+(12, 'DZ', 'Argelia'),
+(13, 'AR', 'Argentina'),
+(14, 'AM', 'Armenia'),
+(15, 'AW', 'Aruba'),
+(16, 'AU', 'Australia'),
+(17, 'AT', 'Austria'),
+(18, 'AZ', 'Azerbaiyán'),
+(19, 'BS', 'Bahamas'),
+(20, 'BH', 'Bahréin'),
+(21, 'BD', 'Bangladesh'),
+(22, 'BB', 'Barbados'),
+(23, 'BY', 'Bielorrusia'),
+(24, 'BE', 'Bélgica'),
+(25, 'BZ', 'Belice'),
+(26, 'BJ', 'Benin'),
+(27, 'BM', 'Bermudas'),
+(28, 'BT', 'Bhután'),
+(29, 'BO', 'Bolivia'),
+(30, 'BA', 'Bosnia y Herzegovina'),
+(31, 'BW', 'Botsuana'),
+(32, 'BV', 'Isla Bouvet'),
+(33, 'BR', 'Brasil'),
+(34, 'BN', 'Brunéi'),
+(35, 'BG', 'Bulgaria'),
+(36, 'BF', 'Burkina Faso'),
+(37, 'BI', 'Burundi'),
+(38, 'CV', 'Cabo Verde'),
+(39, 'KY', 'Islas Caimán'),
+(40, 'KH', 'Camboya'),
+(41, 'CM', 'Camerún'),
+(42, 'CA', 'Canadá'),
+(43, 'CF', 'República Centroafricana'),
+(44, 'TD', 'Chad'),
+(45, 'CZ', 'República Checa'),
+(46, 'CL', 'Chile'),
+(47, 'CN', 'China'),
+(48, 'CY', 'Chipre'),
+(49, 'CX', 'Isla de Navidad'),
+(50, 'VA', 'Ciudad del Vaticano'),
+(51, 'CC', 'Islas Cocos'),
+(52, 'CO', 'Colombia'),
+(53, 'KM', 'Comoras'),
+(54, 'CD', 'República Democrática del Congo'),
+(55, 'CG', 'Congo'),
+(56, 'CK', 'Islas Cook'),
+(57, 'KP', 'Corea del Norte'),
+(58, 'KR', 'Corea del Sur'),
+(59, 'CI', 'Costa de Marfil'),
+(60, 'CR', 'Costa Rica'),
+(61, 'HR', 'Croacia'),
+(62, 'CU', 'Cuba'),
+(63, 'DK', 'Dinamarca'),
+(64, 'DM', 'Dominica'),
+(65, 'DO', 'República Dominicana'),
+(66, 'EC', 'Ecuador'),
+(67, 'EG', 'Egipto'),
+(68, 'SV', 'El Salvador'),
+(69, 'AE', 'Emiratos Árabes Unidos'),
+(70, 'ER', 'Eritrea'),
+(71, 'SK', 'Eslovaquia'),
+(72, 'SI', 'Eslovenia'),
+(73, 'ES', 'España'),
+(74, 'UM', 'Islas ultramarinas de Estados Unidos'),
+(75, 'US', 'Estados Unidos'),
+(76, 'EE', 'Estonia'),
+(77, 'ET', 'Etiopía'),
+(78, 'FO', 'Islas Feroe'),
+(79, 'PH', 'Filipinas'),
+(80, 'FI', 'Finlandia'),
+(81, 'FJ', 'Fiyi'),
+(82, 'FR', 'Francia'),
+(83, 'GA', 'Gabón'),
+(84, 'GM', 'Gambia'),
+(85, 'GE', 'Georgia'),
+(86, 'GS', 'Islas Georgias del Sur y Sandwich del Sur'),
+(87, 'GH', 'Ghana'),
+(88, 'GI', 'Gibraltar'),
+(89, 'GD', 'Granada'),
+(90, 'GR', 'Grecia'),
+(91, 'GL', 'Groenlandia'),
+(92, 'GP', 'Guadalupe'),
+(93, 'GU', 'Guam'),
+(94, 'GT', 'Guatemala'),
+(95, 'GF', 'Guayana Francesa'),
+(96, 'GN', 'Guinea'),
+(97, 'GQ', 'Guinea Ecuatorial'),
+(98, 'GW', 'Guinea-Bissau'),
+(99, 'GY', 'Guyana'),
+(100, 'HT', 'Haití'),
+(101, 'HM', 'Islas Heard y McDonald'),
+(102, 'HN', 'Honduras'),
+(103, 'HK', 'Hong Kong'),
+(104, 'HU', 'Hungría'),
+(105, 'IN', 'India'),
+(106, 'ID', 'Indonesia'),
+(107, 'IR', 'Irán'),
+(108, 'IQ', 'Iraq'),
+(109, 'IE', 'Irlanda'),
+(110, 'IS', 'Islandia'),
+(111, 'IL', 'Israel'),
+(112, 'IT', 'Italia'),
+(113, 'JM', 'Jamaica'),
+(114, 'JP', 'Japón'),
+(115, 'JO', 'Jordania'),
+(116, 'KZ', 'Kazajstán'),
+(117, 'KE', 'Kenia'),
+(118, 'KG', 'Kirguistán'),
+(119, 'KI', 'Kiribati'),
+(120, 'KW', 'Kuwait'),
+(121, 'LA', 'Laos'),
+(122, 'LS', 'Lesotho'),
+(123, 'LV', 'Letonia'),
+(124, 'LB', 'Líbano'),
+(125, 'LR', 'Liberia'),
+(126, 'LY', 'Libia'),
+(127, 'LI', 'Liechtenstein'),
+(128, 'LT', 'Lituania'),
+(129, 'LU', 'Luxemburgo'),
+(130, 'MO', 'Macao'),
+(131, 'MK', 'ARY Macedonia'),
+(132, 'MG', 'Madagascar'),
+(133, 'MY', 'Malasia'),
+(134, 'MW', 'Malawi'),
+(135, 'MV', 'Maldivas'),
+(136, 'ML', 'Malí'),
+(137, 'MT', 'Malta'),
+(138, 'FK', 'Islas Malvinas'),
+(139, 'MP', 'Islas Marianas del Norte'),
+(140, 'MA', 'Marruecos'),
+(141, 'MH', 'Islas Marshall'),
+(142, 'MQ', 'Martinica'),
+(143, 'MU', 'Mauricio'),
+(144, 'MR', 'Mauritania'),
+(145, 'YT', 'Mayotte'),
+(146, 'MX', 'México'),
+(147, 'FM', 'Micronesia'),
+(148, 'MD', 'Moldavia'),
+(149, 'MC', 'Mónaco'),
+(150, 'MN', 'Mongolia'),
+(151, 'MS', 'Montserrat'),
+(152, 'MZ', 'Mozambique'),
+(153, 'MM', 'Myanmar'),
+(154, 'NA', 'Namibia'),
+(155, 'NR', 'Nauru'),
+(156, 'NP', 'Nepal'),
+(157, 'NI', 'Nicaragua'),
+(158, 'NE', 'Níger'),
+(159, 'NG', 'Nigeria'),
+(160, 'NU', 'Niue'),
+(161, 'NF', 'Isla Norfolk'),
+(162, 'NO', 'Noruega'),
+(163, 'NC', 'Nueva Caledonia'),
+(164, 'NZ', 'Nueva Zelanda'),
+(165, 'OM', 'Omán'),
+(166, 'NL', 'Países Bajos'),
+(167, 'PK', 'Pakistán'),
+(168, 'PW', 'Palau'),
+(169, 'PS', 'Palestina'),
+(170, 'PA', 'Panamá'),
+(171, 'PG', 'Papúa Nueva Guinea'),
+(172, 'PY', 'Paraguay'),
+(173, 'PE', 'Perú'),
+(174, 'PN', 'Islas Pitcairn'),
+(175, 'PF', 'Polinesia Francesa'),
+(176, 'PL', 'Polonia'),
+(177, 'PT', 'Portugal'),
+(178, 'PR', 'Puerto Rico'),
+(179, 'QA', 'Qatar'),
+(180, 'GB', 'Reino Unido'),
+(181, 'RE', 'Reunión'),
+(182, 'RW', 'Ruanda'),
+(183, 'RO', 'Rumania'),
+(184, 'RU', 'Rusia'),
+(185, 'EH', 'Sahara Occidental'),
+(186, 'SB', 'Islas Salomón'),
+(187, 'WS', 'Samoa'),
+(188, 'AS', 'Samoa Americana'),
+(189, 'KN', 'San Cristóbal y Nevis'),
+(190, 'SM', 'San Marino'),
+(191, 'PM', 'San Pedro y Miquelón'),
+(192, 'VC', 'San Vicente y las Granadinas'),
+(193, 'SH', 'Santa Helena'),
+(194, 'LC', 'Santa Lucía'),
+(195, 'ST', 'Santo Tomé y Príncipe'),
+(196, 'SN', 'Senegal'),
+(197, 'CS', 'Serbia y Montenegro'),
+(198, 'SC', 'Seychelles'),
+(199, 'SL', 'Sierra Leona'),
+(200, 'SG', 'Singapur'),
+(201, 'SY', 'Siria'),
+(202, 'SO', 'Somalia'),
+(203, 'LK', 'Sri Lanka'),
+(204, 'SZ', 'Suazilandia'),
+(205, 'ZA', 'Sudáfrica'),
+(206, 'SD', 'Sudán'),
+(207, 'SE', 'Suecia'),
+(208, 'CH', 'Suiza'),
+(209, 'SR', 'Surinam'),
+(210, 'SJ', 'Svalbard y Jan Mayen'),
+(211, 'TH', 'Tailandia'),
+(212, 'TW', 'Taiwán'),
+(213, 'TZ', 'Tanzania'),
+(214, 'TJ', 'Tayikistán'),
+(215, 'IO', 'Territorio Británico del Océano Índico'),
+(216, 'TF', 'Territorios Australes Franceses'),
+(217, 'TL', 'Timor Oriental'),
+(218, 'TG', 'Togo'),
+(219, 'TK', 'Tokelau'),
+(220, 'TO', 'Tonga'),
+(221, 'TT', 'Trinidad y Tobago'),
+(222, 'TN', 'Túnez'),
+(223, 'TC', 'Islas Turcas y Caicos'),
+(224, 'TM', 'Turkmenistán'),
+(225, 'TR', 'Turquía'),
+(226, 'TV', 'Tuvalu'),
+(227, 'UA', 'Ucrania'),
+(228, 'UG', 'Uganda'),
+(229, 'UY', 'Uruguay'),
+(230, 'UZ', 'Uzbekistán'),
+(231, 'VU', 'Vanuatu'),
+(232, 'VE', 'Venezuela'),
+(233, 'VN', 'Vietnam'),
+(234, 'VG', 'Islas Vírgenes Británicas'),
+(235, 'VI', 'Islas Vírgenes de los Estados Unidos'),
+(236, 'WF', 'Wallis y Futuna'),
+(237, 'YE', 'Yemen'),
+(238, 'DJ', 'Yibuti'),
+(239, 'ZM', 'Zambia'),
+(240, 'ZW', 'Zimbabue');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(100) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  `contrasenna` varchar(45) NOT NULL,
+  `es_administrador` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `areaswebook`
+--
+ALTER TABLE `areaswebook`
+  ADD PRIMARY KEY (`id_area`);
+
+--
+-- Indices de la tabla `institucion`
+--
+ALTER TABLE `institucion`
+  ADD PRIMARY KEY (`id_institucion`),
+  ADD KEY `area` (`area`),
+  ADD KEY `pais` (`pais`),
+  ADD KEY `ultimo_en_actualizar` (`ultimo_en_actualizar`);
+
+--
+-- Indices de la tabla `institucion_materia`
+--
+ALTER TABLE `institucion_materia`
+  ADD PRIMARY KEY (`id_institucion`,`materia`),
+  ADD KEY `materia` (`materia`);
+
+--
+-- Indices de la tabla `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`id_materia`);
+
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`id_pais`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `areaswebook`
+--
+ALTER TABLE `areaswebook`
+  MODIFY `id_area` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT de la tabla `institucion`
+--
+ALTER TABLE `institucion`
+  MODIFY `id_institucion` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `id_materia` int(100) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `id_pais` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=241;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
